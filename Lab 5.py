31c64e507ec129f043241c6d1ffb6501755a67cd
cfrancine <cfrancinelara@gmail.com>
2025-03-08 23:39:30 +0800

    Add files via upload

---
+++ Lab 5.py
@@ -0,0 +1,12 @@
+age = int(input("Age?:"))
+if age >=65:
+	print("You are a senior")
+elif age >=20:
+	print("You are an adult")
+elif age >=13:
+	print("You are a teen")
+elif age >=0:
+	print("You are a child")
+	
+else:
+	print("Invalid Age")
